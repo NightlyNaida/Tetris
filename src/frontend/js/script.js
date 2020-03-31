e26af1a02b5c9b539fceb613f5578c70c2252853
NightlyNaida <50152021+NightlyNaida@users.noreply.github.com>
2020-03-31 20:17:18 +0300

    Update script.js

--- src/frontend/js/script.js
+++ src/frontend/js/script.js
@@ -5,22 +5,7 @@ var figures = {
         name: 'cube',
         rotates:{
             firstRotate:{
-                firstCube: {
-                    x:6,
-                    y:0,
-                },
-                secondCube:{
-                    x:7,
-                    y:0
-                },
-                thirdCube:{
-                    x:6,
-                    y:1
-                },
-                fourthCube:{
-                    x:7,
-                    y:1
-                }
+                firstCube:{x:4,y:0,},secondCube:{x:5,y:0},thirdCube:{x:4,y:1},fourthCube:{x:5,y:1}
             }
         }
     },
@@ -28,43 +13,69 @@ var figures = {
         name: 'line',
         rotates:{
             firstRotate:{
-                firstCube: {
-                    x:6,
-                    y:0,
-                },
-                secondCube:{
-                    x:6,
-                    y:1
-                },
-                thirdCube:{
-                    x:6,
-                    y:2
-                },
-                fourthCube:{
-                    x:6,
-                    y:3
-                }
+                firstCube:{x:5,y:0,},secondCube:{x:5,y:1},thirdCube:{x:5,y:2},fourthCube:{x:5,y:3}
             },
             secondRotate:{
-                firstCube: {
-                    x:6,
-                    y:0,
-                },
-                secondCube:{
-                    x:7,
-                    y:0
-                },
-                thirdCube:{
-                    x:8,
-                    y:0
-                },
-                fourthCube:{
-                    x:9,
-                    y:0
-                }
+                firstCube:{x:3,y:0,},secondCube:{x:4,y:0},thirdCube:{x:5,y:0},fourthCube:{x:6,y:0}
             },
         },
     },
+    S_figure:{
+        name:'S_figure',
+        rotates:{
+            firstRotate:{
+                firstCube:{x:5,y:0},secondCube:{x:5,y:1},thirdCube:{x:6,y:1},fourthCube:{x:6,y:2}
+            },
+            secondRotate:{
+                firstCube:{x:4,y:1},secondCube:{x:5,y:1},thirdCube:{x:5,y:0},fourthCube:{x:6,y:0}
+            }
+        }
+    },
+    S_figure_flp:{
+        name:"S_figure_flp",
+        rotates:{
+            firstRotate:{
+                firstCube:{x:5,y:0},secondCube:{x:5,y:1},thirdCube:{x:4,y:1},fourthCube:{x:4,y:2}
+            },
+            secondRotate:{
+                firstCube:{x:4,y:0},secondCube:{x:5,y:0},thirdCube:{x:5,y:1},fourthCube:{x:6,y:1}
+            }
+        }
+    },
+    L_figure:{
+        name:"L_figure",
+        rotates:{
+            firstRotate:{
+                firstCube:{x:4,y:0},secondCube:{x:4,y:1},thirdCube:{x:4,y:2},fourthCube:{x:5,y:2}
+            },
+            secondRotate:{
+                firstCube:{x:3,y:2},secondCube:{x:3,y:1},thirdCube:{x:4,y:1},fourthCube:{x:5,y:1}
+            },
+            thirdRotate:{
+                firstCube:{x:4,y:0},secondCube:{x:5,y:0},thirdCube:{x:5,y:1},fourthCube:{x:5,y:2}
+            },
+            fourthRotate:{
+                firstCube:{x:3,y:1},secondCube:{x:4,y:1},thirdCube:{x:5,y:1},fourthCube:{x:5,y:0}
+            }
+        }
+    },
+    L_figure_flp:{
+        name:"L_figure_flp",
+        rotates:{
+            firstRotate:{
+                firstCube:{x:5,y:0},secondCube:{x:5,y:1},thirdCube:{x:5,y:2},fourthCube:{x:4,y:2}
+            },
+            secondRotate:{
+                firstCube:{x:4,y:0},secondCube:{x:4,y:1},thirdCube:{x:5,y:1},fourthCube:{x:6,y:1}
+            },
+            thirdRotate:{
+                firstCube:{x:5,y:0},secondCube:{x:4,y:0},thirdCube:{x:4,y:1},fourthCube:{x:4,y:2}
+            },
+            fourthRotate:{
+                firstCube:{x:4,y:0},secondCube:{x:5,y:0},thirdCube:{x:6,y:0},fourthCube:{x:6,y:1}
+            }
+        }
+    }
 }
 
 var frontGrid = []; //массив из div, которые служат ячейками
@@ -157,7 +168,7 @@ function setCurrentFigure(figure,rotate){
     }
 }
 
-setCurrentFigure(figures['cube'],0);
+setCurrentFigure(figures['L_figure_flp'],3); //стартовая фигура
 
 
 
